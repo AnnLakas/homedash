17fd2056896c01cf3bbb45606b74f1ff2ed918c7
annlakas <marie.hina@iphitech.com>
2023-10-26 14:34:50 +0800

    latest commit

---
+++ app/Enums/ProductTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum ProductTypeEnums : string {
+    case DELIVERABLE = 'deliverable';
+
+    case DOWNLOADBLE = 'downloadble';
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,9 +28,10 @@ class AdminPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Pink,
+                'primary' => Color::Pink, 
             ])
             ->font(family: 'Poppins')
+            ->favicon(url: 'images/logow.png') // fixed favicon
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
